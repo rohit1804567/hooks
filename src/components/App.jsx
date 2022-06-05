4efb89e5d103336a74b8a323cd045fd4fed2f7a4
Rohit Jain <46364854+rohit1804567@users.noreply.github.com>
2022-06-05 10:57:24 +0530

    completed

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,10 +1,20 @@
-import React from "react";
+import React, { useState } from "react";
 
 function App() {
+  let time = new Date().toLocaleTimeString();
+  // console.log(time);
+  setInterval(updateTime, 1000);
+  const [currTime, setCount] = useState(time);
+
+  function updateTime() {
+    time = new Date().toLocaleTimeString();
+    setCount(time);
+  }
+
   return (
-    <div className="container">
-      <h1>TIME</h1>
-      <button>Get Time</button>
+    <div>
+      <h1>{currTime}</h1>
+      <button onClick={updateTime}> Get Time </button>
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -2,12 +2,14 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./components/App";
 
-ReactDOM.render(<App />, document.getElementById("root"));
+let time = new Date().toLocaleTimeString();
+// console.log(time);
+
+ReactDOM.render(<App t={time} />, document.getElementById("root"));
 
 //Challenge:
 //1. Given that you can get the current time using:
-let time = new Date().toLocaleTimeString();
-console.log(time);
+
 //Show the latest time in the <h1> when the Get Time button
 //is pressed.
 
